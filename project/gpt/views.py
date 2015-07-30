80cf76546929184b74a347ff1a4ae24b96a5f821
Chris Maloney <voldrani@gmail.com>
2015-07-30 08:43:31 -0400

    work on jinja view

--- project/gpt/views.py
+++ project/gpt/views.py
@@ -5,8 +5,7 @@ from django.template import RequestContext, loader
 
 
 def home(request):
-    template = loader.get_template('gpt/home.html', using = 'jinja2')
-    return HttpResponse(template.render())
+    return render(request, 'gpt/home.html', using = 'jinja2')
 
 def results(request, result_set_id):
     return HttpResponse("GPT results")
