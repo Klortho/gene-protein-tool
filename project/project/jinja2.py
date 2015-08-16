32cba91878e81b0ef27f5b32292509679f3a4a03
Chris Maloney <voldrani@gmail.com>
2015-08-16 10:18:03 -0400

    Add new data to the result output

--- project/project/jinja2.py
+++ project/project/jinja2.py
@@ -1,8 +1,10 @@
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
 import jinja2
-
 from jinja2 import Environment
+import inspect
+from pprint import pformat
+
 
 @jinja2.contextfunction
 def get_context(c):
@@ -14,6 +16,8 @@ def environment(**options):
         'static': staticfiles_storage.url,
         'url': reverse,
         'context': get_context,
+        'getmembers': inspect.getmembers,
+        'pp': pformat,
     })
     return env
 
